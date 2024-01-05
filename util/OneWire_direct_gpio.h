e16016ec43e7e32f913a97983679f958d3f189ce
DerGraff <renemarcgraff@web.de>
2024-01-05 21:40:25 +0100

    Update OneWire_direct_gpio.h

--- util/OneWire_direct_gpio.h
+++ util/OneWire_direct_gpio.h
@@ -156,6 +156,7 @@ bool directRead(IO_REG_TYPE mask)
 #define DIRECT_WRITE_HIGH(base, mask)   (mask > 0x8000) ? GP16O |= 1 : (GPOS = (mask))
 
 #elif defined(ARDUINO_ARCH_ESP32)
+#include "soc/gpio_struct.h"
 #include <driver/rtc_io.h>
 #define PIN_TO_BASEREG(pin)             (0)
 #define PIN_TO_BITMASK(pin)             (pin)
